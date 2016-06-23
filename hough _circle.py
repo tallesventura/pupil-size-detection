d0643c05b990a34c7111479bb7db5bad0b5242d2
Talles Oliveira de Souza Alves <tallesventura@yahoo.com.br>
2016-06-23 13:54:38 -0500

    added black pixels percentage check

--- hough _circle.py
+++ hough _circle.py
@@ -6,12 +6,14 @@ import os
 import math
 from matplotlib import pyplot as plt
 
-N_IMAGES = 50
+N_IMAGES = 100
 MAX_RADIUS = 80
 MIN_RADIUS = 40
 PROCESS_POS = False
 PROCESS_RADIUS = False
-ENABLE_POST_PROC = True
+ENABLE_POST_PROC = False
+CAPTURE_RATE = 0.5
+PERCENTAGE_BLACK_THRESHOLD = 10
 
 dict_circles = {}
 images_index = []
@@ -70,19 +72,16 @@ def post_process_circles(circles_dict):
     radius_std = int(np.std(list_radius))
 
 
-    print('Radius std: ',radius_std)
+    #print('Radius std: ',radius_std)
 
     prev_radius = radius_mean
     for i in range(len(list_radius)):
         r = list_radius[i]
         c = circles_dict[str(i+1)]
-        #if(abs(radius_list[i] - prev_radius) > radius_std):
         if(abs(r - prev_radius) > radius_std):
-            #print('changed r')
             out_radius[i] = prev_radius
             c[2] = prev_radius
             out_dict[str(i)] = c
-        #prev_radius = radius_list[i]
         prev_radius = r
 
 
@@ -90,28 +89,27 @@ def post_process_circles(circles_dict):
 
 
 def draw_circles(circles,dest_folder):
+	keys = list(circles.keys())
+	for k in keys:
+		img_circles = circles.get(k)
+		src_path = current_folder+"/results/gray scale/"+ k + ".jpg"
+		dest_path = dest_folder+'/'+k + ".jpg"
+		img = cv2.imread(src_path)
 
-    for i in range(len(images_index)):
-        img_circles = circles.get(str(images_index[i]))
-        src_path = current_folder+"/results/binary/"+ str(images_index[i]) + ".jpg"
-        dest_path = dest_folder+'/'+str(images_index[i]) + ".jpg"
-        img = cv2.imread(src_path)
+		if(type(img_circles) is list):
+			for j in range(len(img_circles)):
+				c = (img_circles[j][0],img_circles[j][1])
+				r = img_circles[j][2]
+				cv2.circle(img, c, r, (0, 255, 0), 2)
+		else:
+			c = (img_circles[0],img_circles[1])
+			r = img_circles[2]
+			cv2.circle(img, c, r, (0, 255, 0), 2)
 
-        if(type(img_circles) is list):
-            for j in range(len(img_circles)):
-                c = (img_circles[j][0],img_circles[j][1])
-                r = img_circles[j][2]
-                cv2.circle(img, c, r, (0, 255, 0), 2)
-        else:
-            c = (img_circles[0],img_circles[1])
-            r = img_circles[2]
-            cv2.circle(img, c, r, (0, 255, 0), 2)
-
-        cv2.imwrite(dest_path, img)
+		cv2.imwrite(dest_path, img)
 
 
-
-def count_black_pixels(img, circle):
+def count_pixels(img, circle):
     
     x = circle[1]
     y = circle[0]
@@ -125,97 +123,136 @@ def count_black_pixels(img, circle):
     h = img_box.shape[0]
     w = img_box.shape[1]
 
-    count = 0
+    black = 0
+    white = 0
     for i in range(h):
         for j in range(w):
             if(img_box[i,j] == 0):
-                count+= 1
+                black+= 1
+            else:
+            	white+= 1
 
-    return count
-
+    return black, white
 
 
 def select_circles(circs):
 
-    out = {}
-    for i in range(len(images_index)):
-        img_circles = circs.get(str(images_index[i]))
-        src_path = current_folder+"/results/binary/"+ str(images_index[i]) + ".jpg"
-        img = cv2.imread(src_path,cv2.IMREAD_GRAYSCALE)
-        pCount_list = []
-        circles = []
+	out = {}
+	keys = list(circs.keys())
+	for k in keys:
+		img_circles = circs.get(k)
+		src_path = current_folder+"/results/binary/"+ k + ".jpg"
+		img = cv2.imread(src_path,cv2.IMREAD_GRAYSCALE)
+		blackP_list = []
+		whiteP_list = []
+		circles = []
 
-        for j in range(len(img_circles)):
-            pCount_list.append(count_black_pixels(img,img_circles[j]))
+		for j in range(len(img_circles)):
+			black, white = count_pixels(img,img_circles[j])
+			blackP_list.append(black)
+			whiteP_list.append(white)
 
-        pCount_list = np.array(pCount_list)
+		blackP_list = np.array(blackP_list)
+		index_max = blackP_list.argmax()
+		b = blackP_list[index_max]
+		w = whiteP_list[index_max]
+		total_pixels = b + w 
+		percentage_black = (100*b)/total_pixels
+		if(percentage_black >= PERCENTAGE_BLACK_THRESHOLD):
+			c = img_circles[index_max]
+			out[k] = c
 
-        c = img_circles[pCount_list.argmax()]
-        out[str(images_index[i])] = c
+	return out
 
-    return out
-        
 
+def get_baseline_area(list_areas):
+	n_frames = int(5*(1/CAPTURE_RATE))
+	print("n_frames:",n_frames)
+	l = list_areas[:n_frames]
+	return np.mean(l)
+
+
+def get_percentage_area(list_areas, baseline_area):
+
+	out = []
+	for a in list_areas:
+		out.append((100*a)/baseline_area)
+
+	return out
+
+
+def get_areas(list_radius):
+	out = []
+	for r in list_radius:
+		out.append(3.14 * (r**2))
+
+	return out
 
 
 for i in range(1,N_IMAGES+1):
-    number = str(i)
+	number = str(i)
 
-    image_name = current_folder+"/results/gray scale/"+ number + ".jpg"
-    imageNames.append(image_name)
-    print(image_name)
-    img = cv2.imread(image_name)
+	image_name = current_folder+"/results/gray scale/"+ number + ".jpg"
+	imageNames.append(image_name)
+	print(image_name)
+	img = cv2.imread(image_name)
 
 
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    print(img.shape)
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	print(img.shape)
 
-	
+
 	#dp: This parameter is the inverse ratio of the accumulator resolution to the image resolution (see Yuen et al. for more details). Essentially, the larger the dp gets, the smaller the accumulator array gets.
 	#minDist: Minimum distance between the center (x, y) coordinates of detected circles. If the minDist is too small, multiple circles in the same neighborhood as the original may be (falsely) detected. If the minDist is too large, then some circles may not be detected at all.
 	#param1: Gradient value used to handle edge detection in the Yuen et al. method.
 	#param2: Accumulator threshold value for the cv2.HOUGH_GRADIENT method. The smaller the threshold is, the more circles will be detected (including false circles). The larger the threshold is, the more circles will potentially be returned.
 	#minRadius: Minimum size of the radius (in pixels).
 	#maxRadius: Maximum size of the radius (in pixels).
-    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 2, 100, param1=20, param2=100, minRadius=MIN_RADIUS, maxRadius=MAX_RADIUS)
-    #2, 100, param1=20, param2=100, minRadius=40, maxRadius=80)
-    #2, 30, param1=20, param2=130, minRadius=40, maxRadius=80)
-    #2, 30, param1=20, param2=115, minRadius=40, maxRadius=80)
+	circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 2, 100, param1=20, param2=100, minRadius=MIN_RADIUS, maxRadius=MAX_RADIUS)
+	#2, 100, param1=20, param2=100, minRadius=40, maxRadius=80)
+	#2, 30, param1=20, param2=130, minRadius=40, maxRadius=80)
+	#2, 30, param1=20, param2=115, minRadius=40, maxRadius=80)
 
 
-    cur_img_circles = []
-    if circles is not None:
-        circles = np.uint16(np.around(circles))
-        for c in circles[0,:]:
-            cur_img_circles.append(c)
-            
-        images_index.append(i)
-        dict_circles[number] = cur_img_circles
+	cur_img_circles = []
+	if circles is not None:
+		circles = np.uint16(np.around(circles))
+		for c in circles[0,:]:
+			cur_img_circles.append(c)
+		images_index.append(i)
+		dict_circles[number] = cur_img_circles
+#============================================================================================================
 
 
 circles_raw = dict_circles.copy()
+print("Selecting the best circle")
+circs = select_circles(circles_raw)
+indexes = list(circs.keys())
+print("n_images: ", len(indexes))
+list_radius = get_radius_list(circs)
+list_areas = get_areas(list_radius)
+baseline_area = get_baseline_area(list_areas)
+print("baseline area: ", baseline_area)
+percentage_areas = get_percentage_area(list_areas, baseline_area)
+
 
 if(ENABLE_POST_PROC):
-    circs = select_circles(circles_raw)
-    circles_processed, radius_processed = post_process_circles(circs)
-    draw_circles(circles_processed,dirname)
-    plt.plot(images_index,radius_processed,'r', label='processed')
-    plt.plot(images_index,get_radius_list(circs),'b', label='raw')
-    plt.show()
-    
+	print("post-processing the circles")
+	circles_processed, radius_processed = post_process_circles(circs)
+	print("drawing the circles")
+	draw_circles(circles_processed,dirname)
+	print("plotting the graph")
+	plt.plot(indexes,radius_processed,'r', label='processed')
+	plt.plot(indexes,get_radius_list(circs),'b', label='raw')
 else:
-    circs = select_circles(circles_raw)
-    draw_circles(circles_raw,current_folder+"/results")
-    draw_circles(circs,dirname)
-    #plt.plot(images_index,radius_list,'b', label='raw')
-    
+	print("drawing the circles")
+	draw_circles(circs,dirname)
+	print("plotting the graph")
+	plt.plot(indexes,percentage_areas,'bo', label='raw')
 
-
-'''
-plt.xlabel('Time')
-plt.ylabel('Radius')
+plt.xlabel("Time (sec.)")
+plt.ylabel('% \of Baseline Area')
 plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=1,
-           ncol=2, borderaxespad=0.)
+       ncol=2, borderaxespad=0.)
 plt.show()
-'''
 
